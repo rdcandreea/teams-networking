acf6de1f63b39bc5da9c743c1b142abdfb961be3
Andreea Radac <rdcandreea@gmail.com>
2023-04-10 16:48:09 +0000

    create team request funtion

--- index.js
+++ index.js
@@ -9,21 +9,67 @@ function getTeamsRequest() {
     return r.json();
   });
 }
+
+function createTeamRequest(team) {
+  // POST teams-json/create
+  return fetch("http://localhost:3000/teams-json/create", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(team),
+  }).then((r) => r.json());
+}
+
 function getTeamAsHTML(team) {
   return `<tr>
           <td>${team.promotion}</td>
           <td>${team.members}</td>
           <td>${team.name}</td>
           <td>${team.url}</td>
+          <td></td>
         </tr>
     `;
 }
 
 function showTeams(teams) {
   const html = teams.map(getTeamAsHTML);
-  document.querySelector("table tbody").innerHTML = html.join("");
+  $("table tbody").innerHTML = html.join("");
+}
+
+function $(selector) {
+  return document.querySelector(selector);
+}
+
+function formSubmit(e) {
+  e.preventDefault();
+  console.warn("submit", e);
+  const promotion = $("#promotion").value;
+  const members = $("#members").value;
+  const name = $("#name").value;
+  const url = $("#url").value;
+
+  const team = {
+    promotion: promotion,
+    members: members,
+    name: name,
+    url: url,
+  };
+
+  console.info(team);
+  const r = createTeamRequest(team).then((status) => {
+    console.info("status", status);
+    window.location.reload();
+  });
+}
+//   window.location.reload();
+
+function initEvents() {
+  $("#editForm").addEventListener("submit", formSubmit);
 }
 
 getTeamsRequest().then((teams) => {
   showTeams(teams);
 });
+
+initEvents();
